1328a5f4983e6bca51a275b0f200648cb066adbe
Nikhil Chaudhari <nikhyl777@gmail.com>
2019-03-26 22:18:08 +0530

    File extensions added

--- app/src/main/java/me/nikhilchaudhari/asynkioapp/MainActivity.kt
+++ app/src/main/java/me/nikhilchaudhari/asynkioapp/MainActivity.kt
@@ -2,9 +2,13 @@ package me.nikhilchaudhari.asynkioapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import me.nikhilchaudhari.asynkio.core.async
 import me.nikhilchaudhari.asynkio.core.get
+import me.nikhilchaudhari.asynkio.core.post
+import me.nikhilchaudhari.asynkio.extensions.fileLike
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +18,25 @@ class MainActivity : AppCompatActivity() {
 
         async {
             val response = await { get("https://reqres.in/api/users?page=2") }
-            if (response.statusCode == 200){
-                result_text.text = response.text
+            if (response.statusCode == 200) {
+//                result_text.text = response.text
             }
+        }
+
+        val outputDir = cacheDir
+        val outputFile = File.createTempFile("some-file", "txt", outputDir)
+
+        async {
+            val response = await {
+                post(
+                    "http://mystikolabs.xyz:5000",
+                    files = listOf(outputFile.fileLike("file"))
+                )
+            }
+            Toast.makeText(this@MainActivity, "some", Toast.LENGTH_LONG).show()
+            result_text.text = response.text
+            result_text.text = response.text
+
         }
     }
 
--- asynkio/src/main/java/me/nikhilchaudhari/asynkio/extensions/core_extensions.kt
+++ asynkio/src/main/java/me/nikhilchaudhari/asynkio/extensions/core_extensions.kt
@@ -1,7 +1,19 @@
 package me.nikhilchaudhari.asynkio.extensions
 
+import me.nikhilchaudhari.asynkio.helper.RawFiles
+import java.io.File
+import java.nio.file.Path
+
 class AsyncException(e: Exception, stackTrace: Array<out StackTraceElement>) : RuntimeException(e) {
     init {
         this.stackTrace = stackTrace
     }
 }
+
+fun File.fileLike(name: String = this.name) = RawFiles(name, this)
+
+fun Path.fileLike() = RawFiles(this)
+
+fun Path.fileLike(name: String) = RawFiles(name, this)
+
+fun String.fileLike(name: String) = RawFiles(name, this)
